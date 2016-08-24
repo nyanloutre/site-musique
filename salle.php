1106e676cf049e605f5c221ba2291518d1f2686e
Paul TREHIOU <paul.trehiou@gmail.com>
2016-08-24 15:54:26 +0200

    Ajout carrousel images salle

--- salle.php
+++ salle.php
@@ -94,6 +94,30 @@
         </div>
     </section>
 
+    <section id="carousel-salle" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-salle" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-salle" data-slide-to="1"></li>
+        <li data-target="#carousel-salle" data-slide-to="2"></li>
+    </ol>
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="img/salle/01.jpg" alt="Photo interieur salle">
+        </div>
+    </div>
+    
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-salle" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-salle" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+    </section> <!-- Carousel -->
+    
     <section id="contact">
         <div class="container">
             <div class="row">
