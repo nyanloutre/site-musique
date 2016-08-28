4af1605a1d8017d482087a75da21fd93e79d7217
Paul TREHIOU <paul.trehiou@gmail.com>
2016-08-28 12:49:23 +0200

    Changement id carousel

--- ecole.php
+++ ecole.php
@@ -154,7 +154,7 @@
         </div>
     </section>
     
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <div id="carousel-ecole" class="carousel slide" data-ride="carousel">
     
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
@@ -173,11 +173,11 @@
         </div>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#carousel-ecole" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <a class="right carousel-control" href="#carousel-ecole" role="button" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
