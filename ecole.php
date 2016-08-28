ab18bf54d51b5c17e4a6143f4f21d467899aaa8f
Paul TREHIOU <paul.trehiou@gmail.com>
2016-08-28 12:41:08 +0200

    Illustrations école de musique

--- ecole.php
+++ ecole.php
@@ -153,6 +153,35 @@
             </div>
         </div>
     </section>
+    
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="img/galerie_ecole/01.jpg" alt="Illustration école de musique">
+            </div>
+            <div class="item">
+                <img src="img/galerie_ecole/O2.jpg" alt="Illustration école de musique">
+            </div>
+            <div class="item">
+                <img src="img/galerie_ecole/O3.jpg" alt="Illustration école de musique">
+            </div>
+            <div class="item">
+                <img src="img/galerie_ecole/O4.jpg" alt="Illustration école de musique">
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 
     <section id="contact">
         <div class="container">
