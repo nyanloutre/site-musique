f60f645fe6508442d9449d0806d29a4304650fb9
Paul TREHIOU <paul.trehiou@gmail.com>
2016-08-30 17:23:06 +0200

    Amélioration style listes programme rentrée

--- ecole.php
+++ ecole.php
@@ -88,17 +88,17 @@
                     <p class="text-faded">
                         <div class="row">
                             <b class="col-lg-6">9h30 - 11h</b>
-                            <ul class="col-lg-6">
-                                <li>Atelier d'essai des instruments et d'éveil musical
-                                <li>Planning des cours avec les professeurs
-                                <li>Inscription / réinscription
+                            <ul class="col-lg-6 list-group">
+                                <li class="list-group-item">Atelier d'essai des instruments et d'éveil musical
+                                <li class="list-group-item">Planning des cours avec les professeurs
+                                <li class="list-group-item">Inscription / réinscription
                             </ul>
                         </div>
                         <div class="row">
                             <b class="col-lg-6">vers 11h</b>
-                            <ul class="col-lg-6">
-                                <li>Le "boeuf" des professeurs
-                                <li>Tirage au sort de la tombola gratuite (Gros lot: 1 mois de cours offert pour l'instrument de son choix)
+                            <ul class="col-lg-6 list-group">
+                                <li class="list-group-item">Le "boeuf" des professeurs
+                                <li class="list-group-item">Tirage au sort de la tombola gratuite (Gros lot: 1 mois de cours offert pour l'instrument de son choix)
                             </ul>
                         </div>
                     </p>
