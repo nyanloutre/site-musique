1ce643b5db395446758c3b97999ddf3a9bd7ba41
Paul TREHIOU <paul.trehiou@gmail.com>
2016-11-20 01:08:21 +0100

    Affichage log cacheP2P dans la console

--- vendor/cacheP2P/init.js
+++ vendor/cacheP2P/init.js
@@ -1,1 +1,8 @@
 var cachep2p = new CacheP2P()
+
+cachep2p.on('ready', function(){
+  console.log('This page has been cached!')
+})
+cachep2p.on('message', console.log)
+cachep2p.on('alert', console.log)
+cachep2p.on('success', console.log)
