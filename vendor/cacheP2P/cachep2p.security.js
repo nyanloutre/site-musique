3fd1fda3ed16e83f1615cc9ddb04db4332a43409
Paul TREHIOU <paul.trehiou@gmail.com>
2017-02-04 14:45:13 +0100

    Suppression cache P2P

--- vendor/cacheP2P/cachep2p.security.js
+++
@@ -1,9 +0,0 @@
-document.security_sha1 = {
-    'https://musique-meyenheim.fr/': '901ef94169f31d3ca25cdce2a5babbb08cee8070',
-    'https://musique-meyenheim.fr/index.php': '901ef94169f31d3ca25cdce2a5babbb08cee8070',
-    'https://musique-meyenheim.fr/harmonie.php': '1f784da156f58a6948cacad91617ada29ce1b46a',
-    'https://musique-meyenheim.fr/ecole.php': '2d26257426e3aa85c1dfadeb133239de42f8b966',
-    'https://musique-meyenheim.fr/evenements.php': '984ce188d2720f2fb0388bf7c0ee8de85dc7a12c',
-    'https://musique-meyenheim.fr/direction.php': '45aa5883c0de68534ec17ed1875b901220cb075c',
-    'https://musique-meyenheim.fr/salle.php': 'f43dee9c80dd2b2c9457c95c51c6542344406dcf',
-}
