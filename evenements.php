f833a0d09fb7e67c81a64c224175d4d4e9260c3b
nyanloutre <paul.trehiou@protonmail.com>
2017-09-23 12:51:29 +0200

    Concert de la classe de chant 6 juillet 2017

--- evenements.php
+++ evenements.php
@@ -130,6 +130,39 @@
                     </div>
                 </div>
             </li>
+            
+        <!-- =========================================================== -->
+        <!--      Concert de la classe de chant 6 juillet 2017           -->
+        <!-- =========================================================== -->
+        
+            <li class="timeline">
+                <div class="timeline-badge info"><i class="glyphicon glyphicon-menu-left"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                    <h4 class="timeline-title">Concert de la classe de chant</h4>
+                    <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i>6 juillet 2017</small></br>
+                    <small class="text-muted"><i class="glyphicon glyphicon-time"></i>20h</small></br>
+                    <small class="text-muted"><i class="glyphicon glyphicon-map-marker"></i>Salle de la musique</small></p>
+                    </div>
+                    <div class="timeline-body">
+                        <a href="#pop_concert_6_7_17" data-toggle="modal">
+                            <img src="img/timeline/2017_07_concert_chant_affiche_2.jpg" alt="Affiche 6 juillet" class="img-responsive">
+                        </a>
+                        <hr>
+                        
+                        <!-- Popup -->
+                        <div id="pop_concert_6_7_17" class="modal fade">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-body">
+                                        <img src="img/timeline/2017_07_concert_chant_affiche_2.jpg" class="img-responsive">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
         
         <!-- =========================================================== -->
         <!--        Concert de l'école de musique 20 mai 2017            -->
